eb2a60f0dbe1d5634b2ddf65f68b24f561fd2883
NickeeCoco <mo.bentzinger@gmail.com>
2020-02-13 17:06:02 -0500

    Updates

---
+++ precache-manifest.a3206d60ffe96387807f5f85690a9cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c20cbd2e69d6201d4b9dc7696a5af56",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "19bd2deda2f11f25dd42",
+    "url": "/ToDoApp/static/css/main.15d3ce20.chunk.css"
+  },
+  {
+    "revision": "b885c6ecbcb5ef7f2582",
+    "url": "/ToDoApp/static/js/2.9019a41f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDoApp/static/js/2.9019a41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19bd2deda2f11f25dd42",
+    "url": "/ToDoApp/static/js/main.e293603a.chunk.js"
+  },
+  {
+    "revision": "4ac5a3d7db732ae7d897",
+    "url": "/ToDoApp/static/js/runtime-main.9cfe5332.js"
+  }
+]);
